e505a1dfe0ed2a2ee288f97ca624f060014fd1a8
ootomo <ootomo.h@rio.gsic.titech.ac.jp>
2017-11-17 14:48:42 +0900

    t2

---
+++ job.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#$ -cwd
+#$ -l f_node=1
+#$ -l h_rt=1:00:00
+#$ -N cublasxt
+#$ -M o.h.kisaragi@gmail.com
+#$ -m e
+. /etc/profile.d/modules.sh
+module load cuda
+#cd ${PBS_O_WORKDIR}
+
+./exec
+#./sgemm
+#./hgemm
